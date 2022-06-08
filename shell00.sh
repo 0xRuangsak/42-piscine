6d8cc8d13d5865268738cdb9b3054c065e9f4b6e
0xRuangsak <104247036+0xRuangsak@users.noreply.github.com>
2022-06-08 20:09:00 +0700

    try

--- shell00.sh
+++ shell00.sh
@@ -2,38 +2,48 @@
 mkdir ex01
 touch ./ex01/z
 echo Z > ./ex01/z
+
 #ex02
 mkdir ex02
 #create a file
-mkdir test0
-touch test1
-mkdir test2
-touch test3
-touch test4
+mkdir ./ex02/test0
+touch ./ex02/test1
+mkdir ./ex02/test2
+touch ./ex02/test3
+touch ./ex02/test4
 
 #edit the size
-echo "***" > test1
-echo "" > test3
-echo "*" > test4
+echo "***" > ./ex02/test1
+echo "" > ./ex02/test3
+echo "*" > ./ex02/test4
 
 #link file
-ln test3 test5
-ln -s test0 test6
+ln ./ex02/test3 ./ex02/test5
+ln -s ./ex02/test0 ./ex02/test6
 
 #change the date
-touch -t 202406012047.00 test0
-touch -t 202406012146.00 test1
-touch -t 202406012245.00 test2
-touch -t 202406012344.00 test3
-touch -t 202406012343.00 test4
-touch -t 202406012344.00 test5
-touch -ht 202406012220.00 test6
+touch -t 202406012047.00 ./ex02/test0
+touch -t 202406012146.00 ./ex02/test1
+touch -t 202406012245.00 ./ex02/test2
+touch -t 202406012344.00 ./ex02/test3
+touch -t 202406012343.00 ./ex02/test4
+touch -t 202406012344.00 ./ex02/test5
+touch -ht 202406012220.00 ./ex02/test6
 
 #change permission
-chmod 715 test0
-chmod 714 test1
-chmod 504 test2
-chmod 404 test3
-chmod 641 test4
-chmod 404 test5
-chmod -h 777 test6
+chmod 715 ./ex02/test0
+chmod 714 ./ex02/test1
+chmod 504 ./ex02/test2
+chmod 404 ./ex02/test3
+chmod 641 ./ex02/test4
+chmod 404 ./ex02/test5
+chmod -h 777 ./ex02/test6
+
+#create clean file
+echo "rm -d test0
+rm test1
+rm -d test2
+rm test3
+rm test4
+rm test5
+rm test6" ./ex02/clean.sh
