5d18ef2885bf629471f68497de667005869b145e
0xRuangsak <104247036+0xRuangsak@users.noreply.github.com>
2022-06-08 22:02:47 +0700

    shell00

--- shell00.sh
+++ shell00.sh
@@ -5,9 +5,9 @@ echo Z > ./ex00/z
 #ex01
 mkdir ex01
 touch ./ex01/testShell1100
-echo $(for each in $(seq 2 40); do printf "a"; done) > ./ex01/testShell1100
-touch -a -m -t 202406012342.00 ./ex01/testShell1100
-chmod 455 ./ex01/testShell1100
+echo $(for each in $(seq 2 40); do printf "a"; done) > ./ex01/testShell00
+touch -a -m -t 202406012342.00 ./ex01/testShell00
+chmod 455 ./ex01/testShell00
 tar -cf ./ex01/testShell00.tar ./ex01/testShell00.
 
 #ex02
