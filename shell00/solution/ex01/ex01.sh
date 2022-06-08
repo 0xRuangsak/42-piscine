a3c5e3463bca439b42b690a8e5f7fbea84045a69
0xRuangsak <104247036+0xRuangsak@users.noreply.github.com>
2022-06-08 15:17:17 +0700

    new script

--- shell00/solution/ex01/ex01.sh
+++ shell00/solution/ex01/ex01.sh
@@ -1,4 +1,4 @@
 touch testShell1100
-echo "${$(printf "%-40s" "*")// /*}"
+echo $(for each in $(seq 1 40); do printf "="; done) > testShell1100
 touch -a -m -t 202406012342.00 testShell1100
 chmod 455 testShell1100
