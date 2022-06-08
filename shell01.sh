7e3f95afd07c83fcba9698c69142395b28d5e142
0xRuangsak <104247036+0xRuangsak@users.noreply.github.com>
2022-06-08 19:08:30 +0700

    ex01

---
+++ shell01.sh
@@ -0,0 +1,11 @@
+create_solution()
+{
+    directory=$1
+    name=$2
+    solution=$3
+    mkdir $directory
+    touch ./$directory/$name
+    echo solution > $name
+}
+
+create_solution ex01 print_groups.sh "groups $FT_USER | tr ' ' ',' | tr -d '\n'"
