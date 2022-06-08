7c7978526b28805e9b844d7d654fe00cdd1ae855
0xRuangsak <104247036+0xRuangsak@users.noreply.github.com>
2022-06-08 15:27:09 +0700

    ex02 refac

--- shell00/solution/ex02/ex02.sh
+++ shell00/solution/ex02/ex02.sh
@@ -4,12 +4,12 @@ touch test1
 mkdir test2
 touch test3
 touch test4
-ln test3 test5
-ln -s test0 test6
 
 #edit the size
-echo "${$(printf "%-4s" "*")// /*}" > test1
-echo "${$(printf "%-1s" "*")// /*}" > test3
-echo "${$(printf "%-2s" "*")// /*}" > test4
-echo "${$(printf "%-1s" "*")// /*}" > test5
+echo aaaa > test1
+echo a > test3
+echo aa > test4
 
+#link file
+ln test3 test5
+ln -s test0 test6
