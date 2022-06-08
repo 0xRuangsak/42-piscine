826360a4a17ddab315bf2bc6ba4a5c9a93659a9a
0xRuangsak <104247036+0xRuangsak@users.noreply.github.com>
2022-06-08 22:05:15 +0700

    del

--- shell00.sh
+++ shell00.sh
@@ -4,11 +4,10 @@ echo Z > ./ex00/z
 
 #ex01
 mkdir ex01
-touch ./ex01/testShell00
 echo $(for each in $(seq 2 40); do printf "a"; done) > ./ex01/testShell00
 touch -a -m -t 202406012342.00 ./ex01/testShell00
 chmod 455 ./ex01/testShell00
-tar -cf ./ex01/testShell00.tar ./ex01/testShell00.
+tar -cf ./ex01/testShell00.tar ./ex01/testShell00
 
 #ex02
 mkdir ex02
