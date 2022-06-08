d2cdd660dd67e1a843810d1c40103dedbde6fff7
0xRuangsak <104247036+0xRuangsak@users.noreply.github.com>
2022-06-08 15:21:21 +0700

    size fix

--- shell00/solution/ex01/ex01.sh
+++ shell00/solution/ex01/ex01.sh
@@ -1,4 +1,4 @@
 touch testShell1100
-echo $(for each in $(seq 0 40); do printf "a"; done) > testShell1100
+echo $(for each in $(seq 2 40); do printf "a"; done) > testShell1100
 touch -a -m -t 202406012342.00 testShell1100
 chmod 455 testShell1100
