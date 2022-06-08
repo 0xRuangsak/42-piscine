65ab7c5357f570ee2e65fe6383751d7834b957af
0xRuangsak <104247036+0xRuangsak@users.noreply.github.com>
2022-06-08 15:03:48 +0700

    size update

--- shell00/solution/ex02/ex02.sh
+++ shell00/solution/ex02/ex02.sh
@@ -6,3 +6,9 @@ touch test3
 touch test4
 ln test3 test5
 ln -s test0 test6
+
+#edit the size
+echo "${$(printf "%-4s" "*")// /*}" > test1
+echo "${$(printf "%-1s" "*")// /*}" > test3
+echo "${$(printf "%-2s" "*")// /*}" > test4
+echo "${$(printf "%-1s" "*")// /*}" > test5
