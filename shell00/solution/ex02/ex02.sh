63f0abe18904eb36f68f6a51c0b529c5e3c1fcb1
0xRuangsak <104247036+0xRuangsak@users.noreply.github.com>
2022-06-08 15:33:08 +0700

    edit size

--- shell00/solution/ex02/ex02.sh
+++ shell00/solution/ex02/ex02.sh
@@ -6,10 +6,11 @@ touch test3
 touch test4
 
 #edit the size
-echo aaaa > test1
-echo a > test3
-echo aa > test4
+echo "****" > test1
+echo "*" > test3
+echo "**" > test4
 
 #link file
 ln test3 test5
 ln -s test0 test6
+
