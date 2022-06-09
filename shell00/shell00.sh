997638424a03d396f016c218e3ba041c3b47d14e
0xRuangsak <104247036+0xRuangsak@users.noreply.github.com>
2022-06-09 09:50:37 +0700

    clean

--- shell00/shell00.sh
+++ shell00/shell00.sh
@@ -7,7 +7,7 @@ mkdir ex01
 echo $(for each in $(seq 2 40); do printf "a"; done) > ./ex01/testShell00
 touch -a -m -t 202406012342.00 ./ex01/testShell00
 chmod 455 ./ex01/testShell00
-tar -cf ./ex01/testShell00.tar ./ex01/testShell00
+tar -cf ./ex01/testShell00.tar ./ex01/*
 
 #ex02
 mkdir ex02
